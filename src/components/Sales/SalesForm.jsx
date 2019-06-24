9a5fcb992ed543d80556992b74953d9da3665fce
Caio Batista <caiobatis7@gmail.com>
2019-06-24 19:28:33 -0300

    steps sales form

--- src/components/Sales/SalesForm.jsx
+++ src/components/Sales/SalesForm.jsx
@@ -1,31 +1,59 @@
 import React, { useState, useEffect } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from "react-redux"
-import { Paper, Avatar, Button, FormControl, Input, InputLabel, Grid } from '@material-ui/core'
-import Radio from '@material-ui/core/Radio'
-import RadioGroup from '@material-ui/core/RadioGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import FormLabel from '@material-ui/core/FormLabel'
+import { Paper, Avatar, Button, FormControl, Input, InputLabel, Grid, TextField, MenuItem, Stepper, Step, StepLabel, StepContent } from '@material-ui/core'
 import FileUploader from "react-firebase-file-uploader"
 import { withRouter } from 'react-router-dom'
 import firebase from '../../firebase'
 import {styles} from '../Profile/Profile'
 import withStyles from '@material-ui/core/styles/withStyles'
 import { getUserProfile } from '../../actions/commonsActions';
+import classes from '../Calculator/Calculator.scss'
+
+const currencies = [
+  {
+    value: 'frente',
+    label: 'Frente',
+  },
+  {
+    value: 'fair',
+    label: 'Fair',
+  }
+]
+const _pago = [
+  {
+    value: true,
+    label: 'Sim',
+  },
+  {
+    value: false,
+    label: 'Não',
+  }
+]
 
 function SalesForm (props) {
   const { 
-    classes,
     profile
   } = props
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [access, setAccess] = useState('1')
-  const [progress, setProgress] = useState(0)
-  const [avatar, setAvatar] = useState('')
-  const [avatarURL, setAvatarURL] = useState('')
-  const [isUploading, setIsUploading] = useState(false)
+  const [nome, setNome] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [corretora, setCorretora] = useState('fair')
+  const [praca, setPraca] = useState('')
+  const [quantidade, setQuantidade] = useState('')
+  const [taxa, setTaxa] = useState('')
+  const [total, setTotal] = useState('')
+  const [comprovante, setComprovante] = useState('')
+  const [pago, setPago] = useState(false)
+  const [transporte, setTransporte] = useState('')
+  const [dataEnterga, setDataEnterga] = useState('')
+  const [recebedor, setRecebedor] = useState('')
+  const [custo, setCusto] = useState('')
+  const [pagamento, setPagamento] = useState('')
+  const [obs, setObs] = useState('')
+
+  const [activeStep, setActiveStep] = useState(0)
+  const [sending, setSending] = useState(false)
 
   useEffect(() => {
     // setName(profile.name)
@@ -34,124 +62,230 @@ function SalesForm (props) {
     // setAvatarURL(profile.photoURL)
   }, [profile])
 
-  function handleUploadStart() {
-    setIsUploading(true)
-    setProgress(0)
+  function handleNext() {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
   }
 
-  function handleProgress(progress) {
-    setProgress(progress)
+  function handleBack() {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
   }
-  
-  function handleUploadError(error) {
-    setIsUploading(false)
-  }
-  
-  function handleUploadSuccess(filename) {
-    setAvatar(filename)
-    setProgress(100)
-    setIsUploading(false)
-    firebase
-    .storage
-    .ref('profile')
-    .child(filename)
-    .getDownloadURL()
-    .then(url => setAvatarURL(url))
-  }
+
+  const steps = [
+    {
+      title: 'Dados pessoais',
+      content: (
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <TextField
+                  select
+                  label="Select"
+                  className={classes.textField}
+                  value={corretora}
+                  onChange={e => setCorretora(e.target.value)}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                >
+                  {currencies.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="nome">Nome</InputLabel>
+                <Input id="nome" name="nome" autoComplete="off" autoFocus value={nome} onChange={e => setNome(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="cpf">CPF</InputLabel>
+                <Input id="cpf" name="cpf" autoComplete="off" autoFocus value={cpf} onChange={e => setCpf(e.target.value)} />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </div>
+      )
+    },
+    {
+      title: 'Dados da Venda',
+      content: (
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="quantidade">Quantidade</InputLabel>
+                <Input id="quantidade" name="quantidade" autoComplete="off" autoFocus 
+                value={quantidade} onChange={e => setQuantidade(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="taxa">Taxa</InputLabel>
+                <Input id="taxa" autoComplete="off" autoFocus value={taxa} 
+                onChange={e => setTaxa(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" disabled required fullWidth>
+                <InputLabel htmlFor="total">Total</InputLabel>
+                <Input id="total" name="total" autoComplete="off" autoFocus value={total} 
+                onChange={e => setTotal(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" fullWidth>
+                <InputLabel htmlFor="comprovante">Comprovante</InputLabel>
+                <Input id="comprovante" name="comprovante" autoComplete="off" autoFocus value={comprovante} 
+                onChange={e => setComprovante(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <TextField
+                  select
+                  label="Pago?"
+                  className={classes.textField}
+                  value={pago}
+                  onChange={e => setPago(e.target.value)}
+                  SelectProps={{
+                    MenuProps: {
+                      className: classes.menu,
+                    },
+                  }}
+                >
+                  {_pago.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </div>
+      )
+    },
+    {
+      title: 'Dados da entrega',
+      content: (
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="transporte">Transporte</InputLabel>
+                <Input id="transporte" name="transporte" autoComplete="off" autoFocus 
+                value={transporte} onChange={e => setTransporte(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="dataEnterga">Data de Enterga</InputLabel>
+                <Input id="dataEnterga" autoComplete="off" autoFocus value={dataEnterga} 
+                onChange={e => setDataEnterga(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="recebedor">Recebedor</InputLabel>
+                <Input id="recebedor" name="recebedor" autoComplete="off" autoFocus value={recebedor} 
+                onChange={e => setRecebedor(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" fullWidth>
+                <InputLabel htmlFor="comprovante">Comprovante</InputLabel>
+                <Input id="comprovante" name="comprovante" autoComplete="off" autoFocus value={comprovante} 
+                onChange={e => setComprovante(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" fullWidth>
+                <InputLabel htmlFor="custo">custo</InputLabel>
+                <Input id="custo" name="custo" autoComplete="off" autoFocus value={custo} 
+                onChange={e => setCusto(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3} >
+              <FormControl margin="normal" fullWidth>
+                <InputLabel htmlFor="comprovante">Comprovante</InputLabel>
+                <Input id="comprovante" name="comprovante" autoComplete="off" autoFocus value={comprovante} 
+                onChange={e => setComprovante(e.target.value)} />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </div>
+      )
+    }
+  ]
 
   return (
     <div>
       <form className={classes.form} onSubmit={e => e.preventDefault() && false }>
-        <Grid container spacing={2}>
-          <Grid item xs={3} >
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="name">Nome</InputLabel>
-              <Input id="name" name="name" autoComplete="off" autoFocus value={name} onChange={e => setName(e.target.value)} />
-            </FormControl>
-          </Grid>
-          <Grid item xs={3}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">E-mail</InputLabel>
-              <Input name="email" autoComplete="off" value={email} onChange={e => setEmail(e.target.value)}  />
-            </FormControl>
-          </Grid>
-        </Grid>
-        <Grid container spacing={2}>
-          {
-            access >= 2 && (
-              <Grid item xs={3}>
-                <FormControl component="fieldset" className={classes.formControl}>
-                  <FormLabel component="legend">Nivel de acesso</FormLabel>
-                  <RadioGroup
-                    aria-label="gender"
-                    name="gender2"
-                    value={access}
-                    onChange={(e)=> setAccess(e.target.value)}
-                  >
-                    <FormControlLabel
-                      value='1'
-                      control={<Radio color="primary" />}
-                      label="Nivel 1"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value='2'
-                      control={<Radio color="primary" />}
-                      label="Nivel 2"
-                      labelPlacement="start"
-                    />
-                    <FormControlLabel
-                      value='3'
-                      control={<Radio color="primary" />}
-                      label="Nivel 3"
-                      labelPlacement="start"
-                    />
-                  </RadioGroup>
-                </FormControl>
-              </Grid>
-            )
-          }
-          <Grid item xs={3}>
-            <label>Avatar:</label>
-            {isUploading && <p>Progress: {progress}</p>}
-            <div className={classes.avatar}>
-            {avatarURL && <img src={avatarURL} />}
+        <Paper className={classes.block}>
+          <Stepper activeStep={activeStep} orientation="vertical">
+            {steps.map((label, index) => (
+              <Step key={'label'}>
+                <StepLabel>{label.title}</StepLabel>
+                <StepContent>
+                  {label.content}
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+          <div className={classes.actionsContainer}>
+            <div>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.button}
+              >
+                Voltar
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNext}
+                className={classes.button}
+              >
+                {activeStep === steps.length - 1 ? 'Finalizar venda' : 'Continuar'}
+              </Button>
             </div>
-            {/* <FileUploader
-              accept="image/*"
-              name="avatar"
-              randomizeFilename
-              storageRef={storageRef}
-              onUploadSuccess={handleUploadSuccess}
-              onUploadStart={handleUploadStart}
-              onUploadError={handleUploadError}
-              onUploadSuccess={handleUploadSuccess}
-              onProgress={handleProgress}
-            /> */}
-          </Grid>
-        </Grid>
-        <Grid container spacing={2}>
-          <Grid item xs={3}>
-            <Button
-              disabled={isUploading}
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              onClick={updateProfile}
-              className={classes.submit}
-            >
-              Atualizar perfil
-            </Button>
-          </Grid>
-        </Grid>
+          </div>
+
+          {/* <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <Button
+                disabled={sending}
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={updateProfile}
+                className={classes.submit}
+              >
+                Atualizar perfil
+              </Button>
+            </Grid>
+          </Grid> */}
+        </Paper>
       </form>
     </div>
   )
 
   async function updateProfile() {
   
-    const data = { name, email, avatarURL, access }
+    // const data = { name, email, avatarURL, access }
+    const data = { nome } 
   
     try {
       await firebase.updateProfile(data)
